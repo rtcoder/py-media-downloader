658b46ba4840fc752c202fd0372b9441153b13bb
Dawid Jeż <dawidjez@gmail.com>
2018-10-31 11:20:26 +0100

    python3 support
    python3 support
    add images counter

--- py-media-downloader.py
+++ py-media-downloader.py
@@ -1,10 +1,16 @@
 import time
-import urllib2
-import urllib
+from urllib.request import urlopen
+import urllib.request, urllib.parse, urllib.error
 import re
 import os, sys
-from urlparse import urlparse
+from urllib.parse import urlparse
+from decimal import Decimal
 
+def get_url(str):
+    if (str.startswith('https://') or str.startswith('http://')):
+        return str 
+    else:
+        return 'http://' + str
 
 def uri_validator(x):
     result = urlparse(x)
@@ -14,26 +20,32 @@ def uri_validator(x):
         return False
 
 url = ''
+
 for arg in sys.argv:
     if(arg.startswith('url=')):
-        url = arg[4:]
+        url = get_url(arg[4:])
 
 if(url == '' or uri_validator(url) == False):
-    print 'Error: invalid URL';
+    print('Error: invalid URL')
     exit()
 
-website = urllib2.urlopen(url)
-html = website.read()
+website = urlopen(url)
+html = website.read().decode('utf-8')
 links = re.findall('"((http|ftp)s?://.*?(jpg|png|jpeg|bmp|gif))"', html)
 
 uri = urlparse(url)
 domain = '{uri.netloc}'.format(uri=uri)
 domainfolder = '{uri.path}'.format(uri=uri).strip('/')
 fullpath=domain+'/'+domainfolder
-if(os.path.exists(fullpath) == False):
-    os.makedirs(fullpath, 0777)
 
-for i in links:
-    print i[0]+' is saving as: '+ i[0].split('/')[-1]+' in '+fullpath
-    file = urllib.URLopener()
-    file.retrieve(i[0], fullpath+'/'+i[0].split('/')[-1])
+if(os.path.exists(fullpath) == False):
+    os.makedirs(fullpath, 0o777)
+
+for i in range(0, len(links)):
+    value = links[i]
+    percent = round(Decimal(100 * (i + 1) / len(links)), 2)
+    progress =  '\r' + str(i + 1) + ' / ' + str(len(links)) + '(' + str(percent) + ')'
+    print("\r" + value[0])
+    print(progress, end='\r')
+    file = urllib.request.URLopener()
+    file.retrieve(value[0], fullpath+'/'+value[0].split('/')[-1])
